8e8140ecc117f655e6d9a6a779a3b548d2f51fc8
Aslan <berezg86@gmail.com>
2020-02-16 14:48:51 +0300

    Слайдер справок

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -13,4 +13,44 @@ $(document).ready(function(){
         $(this).toggleClass('active');
         $('.list-dropdown').fadeToggle();
     });
+
+    microloans = new Swiper('.ref-slider', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        loop: true,
+        breakpoints: {
+            320: {
+                slidesPerView: 1
+            },
+            767: {
+                slidesPerView: 1
+            },
+            768: {
+                slidesPerView: 2
+            },
+            991: {
+                slidesPerView: 2
+            },
+            992: {
+                slidesPerView: 3
+            },
+            1365: {
+                slidesPerView: 3
+            },
+            1366: {
+                slidesPerView: 4
+            }
+        },
+        navigation: {
+            nextEl: '.ref-next',
+            prevEl: '.ref-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+
+
 });
